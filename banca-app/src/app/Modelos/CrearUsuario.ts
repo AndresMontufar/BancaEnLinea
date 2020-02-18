117d9ca38c9cc804c6bfc38ece1f2f4a7c1d792e
carlangas97 <43318562+carlangas97@users.noreply.github.com>
2020-02-17 23:10:19 -0600

    registro

---
+++ banca-app/src/app/Modelos/CrearUsuario.ts
@@ -0,0 +1,10 @@
+export interface crearusuario{
+    carnet: number,
+    nombre: string,
+    apellido: string,
+    dpi: number,
+    correo: string,
+    contrasena: string,
+    fecha_nac: string,
+    habilitado: number
+}
