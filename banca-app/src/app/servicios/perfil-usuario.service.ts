5b1991aee050218a37dc7fe716a90c0b70803b25
carlangas97 <43318562+carlangas97@users.noreply.github.com>
2020-02-19 21:49:53 -0600

    CRUD Usuarios

--- banca-app/src/app/servicios/perfil-usuario.service.ts
+++ banca-app/src/app/servicios/perfil-usuario.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import {crearusuario} from '../Modelos/CrearUsuario';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilUsuarioService {
 
-  constructor() { }
-  public VerInfo(perfil){
-    return 'perfil';
+  private API = 'http://54.200.138.95:3000/';
+
+  constructor(private http: HttpClient) { }
+
+  public datos(carnet){
+    return this.http.get(`${this.API}api/user/profile/${carnet}`);
+  }
+
+  registrar(request : crearusuario,carnet){
+    return this.http.put(`${this.API}api/user/update-account/${carnet}`, request);
+  }
+
+
+  public eliminar(carnet){
+    return this.http.put(`${this.API}api/user/disabled_account/${carnet}`,null);
   }
 }
