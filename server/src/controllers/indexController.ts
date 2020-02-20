033044416d7fa6f71bb21d025a723761de9aec64
AndresMontufar97 <amontufarc97@gmail.com>
2020-02-20 00:27:16 -0600

    Update indexController.ts

--- server/src/controllers/indexController.ts
+++ server/src/controllers/indexController.ts
@@ -9,7 +9,7 @@ class IndexController {
         const carnet = req.body.carnet;
         const password = req.body.contrasena;
 
-        const rows = await pool.query('SELECT * from banca.usuario Where carnet = ?', [carnet]);
+        const rows = await pool.query('SELECT * from banca.usuario Where carnet = ? and habilitado = true', [carnet]);
         var validPassword = false;
 
         if(rows.length > 0)
