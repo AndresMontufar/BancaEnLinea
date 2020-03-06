a41888390959ce906a3a5885895d890f10583aa2
carlangas97 <43318562+carlangas97@users.noreply.github.com>
2020-03-05 19:46:49 -0600

    Transferencia

--- banca-app/src/app/componentes/list/list.page.ts
+++ banca-app/src/app/componentes/list/list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GlobalService} from '../../servicios/global.service'
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -20,7 +22,7 @@ export class ListPage implements OnInit {
     'build'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
+  constructor(private global: GlobalService, private router : Router) {
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
@@ -31,6 +33,10 @@ export class ListPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  cancelar(){
+    this.router.navigate([`/home/${this.global.carne}`]);
   }
   // add back when alpha.4 is out
   // navigate(item) {
---
+++ banca-app/src/app/componentes/transferencia/transferencia.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { TransferenciaComponent } from './transferencia.component';
+
+describe('TransferenciaComponent', () => {
+  let component: TransferenciaComponent;
+  let fixture: ComponentFixture<TransferenciaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TransferenciaComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TransferenciaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
