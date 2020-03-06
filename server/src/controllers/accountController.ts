d110996a81e9f12ec599d23b1e44b82727f86001
201602569 <34332044+201602569@users.noreply.github.com>
2020-03-05 22:48:38 -0600

    se agrego deposito
    se simula el deposito de dinero en cuenta bancaria.

--- server/src/controllers/accountController.ts
+++ server/src/controllers/accountController.ts
@@ -8,10 +8,10 @@ class accountController {
         const { carnet } = req.params;
         const cuenta = req.body.no_cuenta;
         const retiro = req.body.retiro;
-        
-        const response = await pool.query('UPDATE banca.cuenta SET saldo = saldo - ?' 
-        +' where usuario_carnet = ? and no_cuenta = ? and estado = 1 and saldo >= ?'
-        ,[retiro, carnet, cuenta, retiro]);
+
+        const response = await pool.query('UPDATE banca.cuenta SET saldo = saldo - ?'
+            +' where usuario_carnet = ? and no_cuenta = ? and estado = 1 and saldo >= ?'
+            ,[retiro, carnet, cuenta, retiro]);
 
         if(response.changedRows > 0)
         {
@@ -19,8 +19,27 @@ class accountController {
         }
         else
         {
-            res.send(false); 
-        }          
+            res.send(false);
+        }
+    }
+
+    public async deposit_account(req: Request, res: Response):Promise<void> {   // deposito de dinero
+        const { carnet } = req.params;
+        const cuenta = req.body.no_cuenta;
+        const deposito = req.body.deposito;
+
+        const response = await pool.query('UPDATE banca.cuenta SET saldo = saldo + ?'
+            +' where usuario_carnet = ? and no_cuenta = ? and estado = 1'
+            ,[deposito, carnet, cuenta]);
+
+        if(response.changedRows > 0)
+        {
+            res.send(true);
+        }
+        else
+        {
+            res.send(false);
+        }
     }
 }
 
--- server/src/routes/accountRoutes.ts
+++ server/src/routes/accountRoutes.ts
@@ -13,6 +13,8 @@ class accountRoutes {
     config(): void{
         
         this.router.put('/withdraw-account/:carnet', AccountController.withdraw_account); // retiro de dinero
+
+        this.router.put('/deposit-account/:carnet', AccountController.deposit_account); // deposito de dinero
     }
 
 }
