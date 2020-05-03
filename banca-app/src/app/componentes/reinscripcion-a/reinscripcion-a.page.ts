59c2f11daf47039bfafb9b3d5991c07ae711b253
carlangas97 <43318562+carlangas97@users.noreply.github.com>
2020-05-02 20:50:33 -0600

    all

--- banca-app/src/app/componentes/reinscripcion-a/reinscripcion-a.page.ts
+++ banca-app/src/app/componentes/reinscripcion-a/reinscripcion-a.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Reinscripciones} from '../../Modelos/reinscripcion';
 import {SuficienciaService} from '../../servicios/suficiencia.service';
 import {GlobalService} from '../../servicios/global.service';
+import {Router} from '@angular/router';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-reinscripcion-a',
@@ -11,14 +13,16 @@ import {GlobalService} from '../../servicios/global.service';
 export class ReinscripcionAPage implements OnInit {
   reinscripciones: Reinscripciones = {
     no_cuenta: 0,
-    cantidad: 0
+    cantidad: 91
   }
 
   cuentas: any = []
 
-  constructor(private global: GlobalService, private suficienciaServices: SuficienciaService) { }
+  constructor(private global: GlobalService, private suficienciaServices: SuficienciaService,
+              private router : Router, public alertController: AlertController) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await delay(500);
     this.suficienciaServices.getCuentas(this.global.carne).subscribe(
         res => {
           this.cuentas = res;
@@ -26,9 +30,15 @@ export class ReinscripcionAPage implements OnInit {
         },
         err => console.error(err)
     )
+    await delay(500);
   }
 
   pagar(){
-
+    this.router.navigate([`/home/${this.global.carne}`])
+    console.log(this.reinscripciones)
   }
+
+}
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
 }
